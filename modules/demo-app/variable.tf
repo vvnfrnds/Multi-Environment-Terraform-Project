68a23632f9a4456327c0c999702b00990a96ffb9
vvnfrnds <119041296+vvnfrnds@users.noreply.github.com>
2024-08-27 12:58:39 +0400

    Create variable.tf

---
+++ modules/demo-app/variable.tf
@@ -0,0 +1,23 @@
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "instance_name" {
+  type = string
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+variable "table_name" {
+  type = string
+}
+
+variable "env_name" {
+  type = string
+}
