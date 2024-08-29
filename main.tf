11a06d43b32f5d1d97afce053a72a52335391781
vvnfrnds <119041296+vvnfrnds@users.noreply.github.com>
2024-08-29 10:14:59 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,33 @@
+#DEV INFRA
+module "dev-demo-app" {
+  source = "./modules/demo-app/"
+  env_name = "dev"
+  instance_type = "t2.micro"
+  ami_id = "ami-0bb84b8ffd87024d8"
+  instance_name = "demo-instance"
+  bucket_name = "demo-bucket"
+  table_name = "demo.dynamodb"
+ }
+
+#QA INFRA
+module "qa-demo-app" {
+  source = "./modules/demo-app/"
+  env_name = "qa"
+  instance_type = "t2.small"
+  ami_id = "ami-0bb84b8ffd87024d8"
+  instance_name = "demo-instance"
+  bucket_name = "demo-bucket"
+  table_name = "demo.dynamodb"
+ }
+
+#PROD INFRA
+module "prod-demo-app" {
+  source = "./modules/demo-app/"
+  env_name = "prod"
+  instance_type = "t2.medium"
+  ami_id = "ami-0fe630eb857a6ec83"
+  instance_name = "demo-instance"
+  bucket_name = "demo-bucket"
+  table_name = "demo.dynamodb"
+ }
 
