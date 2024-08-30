aa86d25343ff5ccdc356a3daadcfaa61c8558ffd
vvnfrnds <119041296+vvnfrnds@users.noreply.github.com>
2024-08-30 12:40:22 +0400

    Update s3.tf

--- modules/demo-app/s3.tf
+++ modules/demo-app/s3.tf
@@ -1,3 +1,9 @@
 resource "aws_s3_bucket" "bucketwala" {
-  bucket = "${var.env_name}-${var.bucket_name}"
+  bucket = "${var.env_name}-${var.bucket_name}-${random_string.suffix.result}"   //Specify bucket resource details
+}
+
+resource "random_string" "suffix" {  //Resource for adding S3Bucket names automatically
+ length = 6
+  special = false
+ upper = false
 }
